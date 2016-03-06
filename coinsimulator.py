909fb2fe82868790e477a86d744469ec608cd780
Nate Hefner <natehefner@Nathans-Air.fios-router.home>
2016-03-06 11:30:26 -0500

    asks user what the coin will be, gives them feedback

--- coinsimulator.py
+++ coinsimulator.py
@@ -1,11 +1,18 @@
 import random
 coin = random.randint(0,1)
 
-name = raw_input("What is your name?")
+name = raw_input("What is your name? ")
 
-print("Hello {}.".format(name))
+guess = raw_input("Hello {}. What do you think the coin will be? ".format(name))
 
-if coin == 0:
-	print("The coin is tails.")
-if coin == 1:
-	print("The coin is heads.")
+if coin == 0 and guess == "Tails":
+	print("Great job guessing, {}. The coin is tails.".format(name))
+	
+elif coin == 0 and guess != "Tails":
+	print("Sorry, {}. You guessed wrong. The coin is tails.".format(name))
+	
+elif coin == 1 and guess == "Heads":
+	print("Great job guessing, {}. The coin is heads.".format(name))
+	
+elif coin == 1 and guess != "Heads":
+	print("Sorry, {}. You guessed wrong. The coin is heads.".format(name))
