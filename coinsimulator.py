06a995e788e9834852bb2340896e7e4d84484c62
Nate Hefner <natehefner@Nathans-MacBook-Air.local>
2016-03-07 13:08:15 -0500

    fully improved coin simulator

--- coinsimulator.py
+++ coinsimulator.py
@@ -4,15 +4,16 @@ coin = random.randint(0,1)
 name = raw_input("What is your name? ")
 
 guess = raw_input("Hello {}. What do you think the coin will be? ".format(name))
+guess = guess.lower()
 
-if coin == 0 and guess == "Tails":
+if coin == 0 and guess == "tails":
 	print("Great job guessing, {}. The coin is tails.".format(name))
 	
-elif coin == 0 and guess != "Tails":
+elif coin == 0 and guess != "tails":
 	print("Sorry, {}. You guessed wrong. The coin is tails.".format(name))
 	
-elif coin == 1 and guess == "Heads":
+elif coin == 1 and guess == "heads":
 	print("Great job guessing, {}. The coin is heads.".format(name))
 	
-elif coin == 1 and guess != "Heads":
+elif coin == 1 and guess != "heads":
 	print("Sorry, {}. You guessed wrong. The coin is heads.".format(name))
